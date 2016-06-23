83b90eaef798a34bf4b7bd0a86fe10dbc56b826f
Dima Murashko <snowkam@bk.ru>
2016-06-23 12:33:38 +0300

    В линукс установил кнопку при клике меняю надпись

--- app/src/main/java/com/example/dmurashko/mygitwindowstolinux/MainActivity.java
+++ app/src/main/java/com/example/dmurashko/mygitwindowstolinux/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dmurashko.mygitwindowstolinux;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /// проект созданный в windows
+
+        // это я делаю в линух
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setText("Linux");
+            }
+        });
     }
 }
